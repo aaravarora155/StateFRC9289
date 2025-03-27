cd5e6b7abfe2296a60a657f19ad612ea9b494f6c
Aditya-2204 <william1binki@gmail.com>
2025-03-27 17:36:17 -0500

    Add periodic method to Elevator subsystem for dashboard speed display. kBrake

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,7 +1,12 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,6 +22,20 @@ public class Elevator extends SubsystemBase {
         DAMPENING = 0.5;
         motor_1 = new SparkMax(MotorMiscConstants.ELEVATOR_MOTOR_ID_1, MotorType.kBrushless);
         motor_2 = new SparkMax(MotorMiscConstants.ELEVATOR_MOTOR_ID_2, MotorType.kBrushless);
+
+        SparkMaxConfig config = new SparkMaxConfig();
+        config
+            .inverted(true)
+            .idleMode(IdleMode.kBrake);
+        config.encoder
+            .positionConversionFactor(1000)
+            .velocityConversionFactor(1000);
+        config.closedLoop
+            .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+            .pid(1.0, 0.0, 0.0);
+            
+        motor_1.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        motor_1.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
     public void move(double speed){
@@ -28,6 +47,11 @@ public class Elevator extends SubsystemBase {
     public void stop(){
         motor_1.set(0);
         motor_2.set(0);
+    }
+
+    @Override
+    public void periodic(){
+        SmartDashboard.putNumber("Elevator Speed", motor_1.get());
     }
 }
 
